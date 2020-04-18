d5a2d795b3c985a74a82cb511140da7d8035773e
Fikasentani <mpilo.dlamini@yahoo.com>
2020-04-18 02:12:57 +0200

    adding put route

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
     next();
 });
 
@@ -78,6 +78,26 @@ app.delete("/api/laptops/:id", (req, res, next) => {
     res.status(200).json({
         message: 'laptop deleted'
     });
+})
+
+app.put("/api/laptops/:id", (req, res, next) => {
+    const laptop = new Laptop({
+        _id: req.body._id,
+        brand: req.body.brand,
+        model:req.body.model,
+        ram: req.body.ram,
+        processor: req.body.processor,
+        storage: req.body.storage,
+        condition: req.body.condition,
+        misc: req.body.misc,
+        quantity: req.body.quantity,
+        price: req.body.price,
+        image: req.body.image
+    });
+   Laptop.updateOne({_id: req.params.id}, laptop).then(result => {
+       console.log(result);
+       res.status(200).json({message: 'Update successful'});
+   }) 
 })
 
 const port = 3000;
